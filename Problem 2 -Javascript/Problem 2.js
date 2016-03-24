619b5f55634c9973bde1cd8df502f2f02fb6a004
Tyler Lawson <lawsontyler@utexas.edu>
2016-03-24 12:45:14 -0500

    Added some code for interator class in problem 2

--- Problem 2 -Javascript/Problem 2.js
+++ Problem 2 -Javascript/Problem 2.js
@@ -1,3 +1,87 @@
 /**
  * Created by Tyler on 3/24/2016.
  */
+"use strict";
+
+var list = function() {
+    var list = function () {
+        function Node(data) {
+            this.data = data;
+            this.next = null;
+
+        }
+
+        var l = {
+            length: 0,
+            currentNode: null,
+            head: new Node(null),
+            add: function(e) {
+                if (l.currentNode === null) { // This is true the first time
+                    l.head.data = e;
+                    l.currentNode = new Node(null);
+                    l.head.next = l.currentNode;
+                    l.length++;
+                }
+                else {
+                    l.currentNode.data = e;
+                    var node = new Node(null);
+                    l.currentNode.next = node;
+                    l.currentNode = node;
+                    l.length++;
+                }
+            },
+        };
+
+        var F = function () {
+        };
+        var f = new F();
+
+        // public data
+        f.run = function (e) {
+            return l[e];
+        };
+        f.first = f.car = function () {
+            return l.head.data
+        };
+        f.rest = f.cdr = function () {
+            if(l.length > 0) {
+                l.head = l.head.next;
+                l.length--;
+            }
+            return this;
+        }
+        f.concat = f.cons = function(e){
+            if (typeof e === 'string' || e instanceof String) {l.add(e);}
+            else {
+                var n = e.run('head');
+                for(var i = 0; i < e.run('length'); i++) {
+                    l.add(n.data);
+                    n = n.next;
+                }
+            }
+        }
+        f.length = function(){return l.length};
+        f.map = function(f){
+            if (f instanceof Function) {
+                var n = l.head;
+                for(var i = 0; i < l.length; i++) {
+                    n.data = f(n.data);
+                    n = n.next;
+                }
+            }
+        }
+        f.iterator = function() {
+
+            l.currentNode = l.currentNode.next
+            var lastElementReturned = 'l.currentNode.next.getData()'
+            return l.currentNode.getData()
+
+
+
+        }
+
+        return f;
+    }();
+    return list;
+};
+
